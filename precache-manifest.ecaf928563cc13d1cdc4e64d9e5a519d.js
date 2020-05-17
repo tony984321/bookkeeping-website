effed208f378f643c6f679975b93daeb1fe1fece
TonyWang <448574653@qq.com>
2020-05-17 23:07:18 +0800

    deploy

---
+++ precache-manifest.ecaf928563cc13d1cdc4e64d9e5a519d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041fe560be3621448795",
+    "url": "/bookkeeping-website/css/app.d1882850.css"
+  },
+  {
+    "revision": "efa9301fc51c5faa62f0",
+    "url": "/bookkeeping-website/css/chunk-vendors.92e56d25.css"
+  },
+  {
+    "revision": "5db4977844e657c225867ad56d8d022f",
+    "url": "/bookkeeping-website/fonts/iconfont.5db49778.eot"
+  },
+  {
+    "revision": "8d9904f12d218ea22d71d1cf10e95675",
+    "url": "/bookkeeping-website/fonts/iconfont.8d9904f1.woff"
+  },
+  {
+    "revision": "a155f3a6ba9e00b734eba54be1f36b4d",
+    "url": "/bookkeeping-website/fonts/iconfont.a155f3a6.ttf"
+  },
+  {
+    "revision": "831d4bb4c540ad9f828232dc20451bd0",
+    "url": "/bookkeeping-website/img/default-avatar.831d4bb4.png"
+  },
+  {
+    "revision": "af86fec15a4d02cdc8118ab79afad48e",
+    "url": "/bookkeeping-website/img/iconfont.af86fec1.svg"
+  },
+  {
+    "revision": "2d17192926f198da6a09a8270b41edf9",
+    "url": "/bookkeeping-website/index.html"
+  },
+  {
+    "revision": "041fe560be3621448795",
+    "url": "/bookkeeping-website/js/app.08f0017a.js"
+  },
+  {
+    "revision": "efa9301fc51c5faa62f0",
+    "url": "/bookkeeping-website/js/chunk-vendors.a933bd58.js"
+  },
+  {
+    "revision": "d1b00465103b8de3edb45d0584a5e5c4",
+    "url": "/bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookkeeping-website/robots.txt"
+  }
+]);
